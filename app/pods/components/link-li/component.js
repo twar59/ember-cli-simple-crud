6bb89309a7482596d3877ebef75d2bb0ba889582
Tom White <twar59@gmail.com>
2014-08-24 07:24:13 -0600

    add the link-li component for the side-nav

---
+++ app/pods/components/link-li/component.js
@@ -0,0 +1,7 @@
+export default Em.Component.extend({
+    tagName: 'li',
+    classNameBindings: ['active'],
+    active: function() {
+        return this.get('childViews').anyBy('active');
+    }.property('childViews.@each.active')
+});
